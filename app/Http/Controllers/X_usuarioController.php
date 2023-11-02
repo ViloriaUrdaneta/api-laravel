9d108bf759697b9048ec4032253f690cbae723e3
Miguel <miguel.viloria.247@gmail.com>
2023-11-02 15:14:57 -0300

    login tokin

--- app/Http/Controllers/X_usuarioController.php
+++ app/Http/Controllers/X_usuarioController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\X_usuario;
+use Illuminate\Support\Facades\Log;
+
 
 
 class X_usuarioController extends Controller
@@ -23,25 +25,29 @@ class X_usuarioController extends Controller
         $xusuario->user_pass = Hash::make($request->password);
         $xusuario->save();
 
-        //Auth::login($xusuario);
+        Auth::login($xusuario);
+        $token = $xusuario->createToken("example");
         
-        return response()->json($xusuario);
+        return response()->json($token);
     }
 
 
     public function login(Request $request){
         
         $response = ["status"=>0, "msg"=>""];
-        $data = json_decode($request->getContent());
-        $xusuario = X_usuario::where('user_email', $data->email)->first();
+
+        $user_email = $request->email;
+        $user_pass = $request->password;
+        
+        $xusuario = X_usuario::where('user_email', $user_email)->first();
 
         if($xusuario){
-            if(Hash::check($data->password, $xusuario->user_pass)){
-                //$token = $xusuario->createToken("example");
-
+            if(Hash::check($user_pass, $xusuario->user_pass)){
+                Auth::login($xusuario);
+                $token = $xusuario->createToken("example");
                 $response["status"] = 1;
-                //$response["msg"] = $token->plainTextToken;
-                $response["msg"] = $xusuario;
+                $response["msg"] = $token->plainTextToken;
+                
             }else{
                 $response["msg"] = "Invalid pasword";
             }
@@ -59,15 +65,3 @@ class X_usuarioController extends Controller
     }
 }
 
-
-/**echo 'hola';
-        
-        $xusuario = new X_usuario();
-        
-        $xusuario->user_email = $request->email;
-        $xusuario->user_pass = Hash::make($request->password);
-        $xusuario->save();
-
-        Auth::login($xusuario);
-        
-        return response()->json($xusuario); */
--- app/Models/X_usuario.php
+++ app/Models/X_usuario.php
@@ -4,15 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 
-class X_usuario extends Model
+
+class X_usuario extends Authenticatable
 {
-    use HasFactory;
 
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'user_email',
         'user_pass'
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    protected $primaryKey = 'user_id';
+
+    protected $morphClass = 'X_usuario';
+
+    public function tokens()
+    {
+        return $this->morphMany(PersonalAccessToken::class, 'tokenable', 'tokenable_type', 'tokenable_id');
+    }
 
 }
